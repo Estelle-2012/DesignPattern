8cee915d2a8f66c13553d33f5fa06e672cd57f5e
wb.huhuimin <wb.huhuimin@mesg.corp.netease.com>
2019-02-21 17:01:28 +0800

    开闭原则

---
+++ src/com/estelle/test/ocp/ICourse.java
@@ -0,0 +1,11 @@
+package com.estelle.test.ocp;
+
+public interface ICourse {
+    Integer getId();
+
+    String getName();
+
+    Double getPrice();
+
+
+}
---
+++ src/com/estelle/test/ocp/JavaDiscountCourse.java
@@ -0,0 +1,14 @@
+package com.estelle.test.ocp;
+
+//打折价格
+public class JavaDiscountCourse extends JavaCourse {
+    
+    public JavaDiscountCourse(Integer id, String name, Double price) {
+        super(id, name, price);
+    }
+
+    @Override
+    public Double getPrice() {
+        return super.getPrice() * 0.8;
+    }
+}
